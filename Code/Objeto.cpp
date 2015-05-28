7fca264f2ef8467c2fcc51bf3c0df5b5cbe57a5c
fulano7 <irvm@cin.ufpe.br>
2015-05-28 20:36:52 -0300

    Adicionando implementacao inicial de Objeto

---
+++ Code/Objeto.cpp
@@ -0,0 +1,11 @@
+#include "Objeto.h"
+
+Objeto::Objeto(const vector<float*>* vs, const vector<float*>* ns, const vector<int*>* fs) : vertices(*vs), normais(*ns), faces(*fs)
+{}
+
+Objeto::~Objeto()
+{
+	for (int i = 0; i < vertices.size(); i++) delete[] vertices.at(i);
+	for (int i = 0; i < normais.size(); i++) delete[] normais.at(i);
+	for (int i = 0; i < faces.size(); i++) delete[] faces.at(i);
+}
--- Code/Objeto.h
+++ Code/Objeto.h
@@ -1,5 +1,9 @@
+#ifndef _OBJETO_H_
+#define _OBJETO_H_
+
 #include <vector>
 #include <string>
+
 #include "common.h"
 
 using std::vector;
@@ -10,9 +14,11 @@ class Objeto
 public:
 	vector <float*> vertices;
 	vector <float*> normais;
-	vector <unsigned int *> faces;
+	vector <int*> faces;
 	string nome;
 
-	Objeto(const float *vertices, const float* normais, const unsigned int * faces);
+	Objeto(const vector<float*>* vs, const vector<float*>* ns, const vector<int*>* fs);
 	~Objeto();
 };
+
+#endif // _OBJETO_H_
