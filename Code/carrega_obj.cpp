7a68c2fb79e7375ec319d145093473902b9b9902
fulano7 <irvm@cin.ufpe.br>
2015-05-29 17:59:06 -0300

    Adicionando faces sem normais

--- Code/carrega_obj.cpp
+++ Code/carrega_obj.cpp
@@ -6,47 +6,85 @@ conforme especificado, se destinara a ler estes arquivos .obj:
 http://cin.ufpe.br/~marcelow/Marcelow/arquivos_obj.html
 outros .obj podem nao ser suportados, e este algoritmo nao detecta erros no arquivo.
 */
+
+int ocorrencias(const char* palavra, const char caractere)
+{
+	int resposta = 0;
+	int i = 0;
+	char atual = palavra[i];
+	while (atual != '\0')
+	{
+		if (atual == caractere) resposta++;
+		atual = palavra[++i];
+	}
+	return resposta;
+}
+
 int carregar_obj(Objeto*& array_de_objetos, const char *caminho_arquivo)
 {
 	array_de_objetos = new Objeto[MAX_OBJS_ARQUIVO]();
 	std::ifstream arquivo (caminho_arquivo, std::ifstream::in);
 	char linha[MAX_CHARS_LINHA];
+	
 	bool precisa_normais = true;// variavel que diz se precisa calcular as normais
-	int indice = 0; // armazena a proxima posicao livre do array recebido como parametro.
+
+	int indice = -1; // armazena a proxima posicao livre do array recebido como parametro.
 	// um mesmo arquivo pode ter varios objetos.
 	// se o arquivo tiver mais de um objeto, vamos adicionando os seguintes ao array.
-	while (arquivo.getline(linha, MAX_CHARS_LINHA))
+
+	// primeira chamada de leitura do arquivo.
+	arquivo.getline(linha, MAX_CHARS_LINHA);
+
+	// enquanto o arquivo nao terminar
+	while (!arquivo.eof())
 	{
 		#ifdef DEBUG
 		std::cout << linha << std::endl;
 		#endif
-		if (linha[0] == 'v' && linha[1] == 'n')
+		if (linha[0] == 'v' && linha[1] == 'n') // normal
 		{
-			// normal
+			precisa_normais = false;
 		}
-		else if (linha[0] == 'v')
+		else if (linha[0] == 'v') // vertice
 		{
-			precisa_normais = true;
-			char* proximo = strtok(linha, " ");
+			precisa_normais = true; // resetando esta variavel
+			indice++; // avancando no array
+			char* proximo; // gamb
 			do
 			{
+				// colocando coordenadas x, y e z
+				proximo = strtok(linha, " ");
 				array_de_objetos[indice].vertices.push_back(new float[3]);
-				array_de_objetos[indice].vertices.back()[0] = strtof((strtok(NULL, " ")), NULL);
-				array_de_objetos[indice].vertices.back()[1] = strtof((strtok(NULL, " ")), NULL);
-				array_de_objetos[indice].vertices.back()[2] = strtof((strtok(NULL, " ")), NULL);
+				array_de_objetos[indice].vertices.back()[0] = strtof((strtok(NULL, " ")), &proximo);
+				array_de_objetos[indice].vertices.back()[1] = strtof((strtok(NULL, " ")), &proximo);
+				array_de_objetos[indice].vertices.back()[2] = strtof((strtok(NULL, " ")), &proximo);
 				arquivo.getline(linha, MAX_CHARS_LINHA);
 			}while (linha[0] == 'v');
-			delete[] proximo;
-			indice++;
-			// vertice
+
 		}
-		else if (linha[0] == 'f')
+		else if (linha[0] == 'f') // face
 		{
-			// face (tratar com e sem normal!)
+			
+			if (strstr(linha, "//") != NULL) // com normal.
+			{
+				
+			}
+			else // sem normal.
+			{
+				char* proximo; // gamb
+				do
+				{
+					// colocando faces
+					proximo = strtok(linha, " ");
+					int num_faces = ocorrencias(linha, ' ');
+					array_de_objetos[indice].faces.push_back(new int[num_faces]);
+					for (int i = 0; i < num_faces; i++) array_de_objetos[indice].faces.back()[i] = atoi((strtok(NULL, " "))); // cuidado com atoi
+					arquivo.getline(linha, MAX_CHARS_LINHA);
+				} while (linha[0] == 'f');
+			}
 		}
-		else if (linha[0] == '#'); // comentario
-		else
-		{} // nao suportado; nao tratado
+		else if (linha[0] == '#')arquivo.getline(linha, MAX_CHARS_LINHA); // comentario
+		else arquivo.getline(linha, MAX_CHARS_LINHA); // nao suportado; nao tratado
 	}
-	return 0; // para poder compilar por enquanto
+	return (indice+1);
 }
--- Code/carrega_obj.h
+++ Code/carrega_obj.h
@@ -19,7 +19,15 @@ outros .obj podem nao ser suportados.
 #define MAX_OBJS_ARQUIVO 2
 
 /*
-carrega um arquivo .obj cujo caminho eh caminho_arquivo.
+retorna quantas vezes o caractere 'caractere' aparece na palavra 'palavra'
+*/
+int ocorrencias(const char* palavra, const char caractere);
+
+/*
+carrega um arquivo .obj cujo caminho eh caminho_arquivo e retorna
+um array com os objetos que estavam no arquivo.
+o numero maximo de objetos suportado num arquivo .obj eh fixado
+em MAX_OBJS_ARQUIVO.
 */
 int carregar_obj(Objeto*& array_de_objetos, const char *caminho_arquivo);
 
