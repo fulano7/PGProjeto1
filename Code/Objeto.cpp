0df8fac40f1090131d7ffddbcfaad71a4567a5e9
fulano7 <irvm@cin.ufpe.br>
2015-05-31 13:13:15 -0300

    Continuando

--- Code/Objeto.cpp
+++ Code/Objeto.cpp
@@ -97,8 +97,8 @@ int Objeto::carregar_obj(Objeto*& array_de_objetos, const char *caminho_arquivo)
 						proximo = strtok(linha, " ");
 						array_de_objetos[indice].faces.push_back(new int[(2*num_faces) + 1]);
 						array_de_objetos[indice].faces.back()[0] = num_faces;
-						for (int i = 1; i <= num_faces; i++)
-							// TODO esta leitura array_de_objetos[indice].faces.back()[i] = atoi((strtok(NULL, " "))); // cuidado com atoi
+						for (int i = 1; i <= (2*num_faces)+1; i+=2)
+							//TODO esta leitura array_de_objetos[indice].faces.back()[i] = atoi((strtok(NULL, " "))); // cuidado com atoi
 						arquivo.getline(linha, MAX_CHARS_LINHA);
 					} while (linha[0] == 'f');
 				}
