cd16a442bed49ac5aebc45bec7d2a3440f8e48cf
fulano7 <irvm@cin.ufpe.br>
2015-06-07 16:44:32 -0300

    Preparando para equivalências com funcoes nao autorizadas

---
+++ Code/equivalentes.cpp
@@ -0,0 +1,24 @@
+#include "equivalentes.h"
+
+void equivalentes::nosso_gluLookAt(
+	double eyeX, double eyeY, double eyeZ,
+	double centerX, double centerY, double centerZ,
+	double upX, double upY, double upZ)
+{
+	
+}
+
+void equivalentes::nosso_glTranslatef(float x, float y, float z)
+{
+
+}
+
+void equivalentes::nosso_glRotatef(float angle, float x, float y, float z)
+{
+
+}
+
+void equivalentes::nosso_glScalef(float x, float y, float z)
+{
+
+}
