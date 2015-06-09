d76378a2869a83ba67e9d626ce0ca889d2283dab
fulano7 <irvm@cin.ufpe.br>
2015-06-09 15:24:41 -0300

    Camera

--- Code/Camera.h
+++ Code/Camera.h
@@ -1,10 +1,22 @@
 #ifndef _CAMERA_H_
 #define _CAMERA_H_
 
-class Camera {
-	
-public:
-	float** geraMatrizRotacao();
+#include "common.h"
+
+namespace camera {
+// atencao! todas as matrizes sao column-major!
+	void nossoLookat(
+		float eyeX, float eyeY, float eyeZ,
+		float centerX, float centerY, float centerZ,
+		float upX, float upY, float upZ);
+	void nossoRotate(float angle, float x, float y, float z);
+	void nossoTranslate(float x, float y, float z);
+	float extrinsic[] = {1.f, 0.f, 0.f, 0.f,
+						0.f, 1.f, 0.f, 0.f,
+						0.f, 0.f, 1.f, 0.f,
+						0.f, 0.f, 0.f, 1.f };
+	void produto_vetorial(float* prod, float* v1, float* v2);
+	void normalizar(float* v);
 };
 
 #endif// _CAMERA_H_
---
+++ Code/camera.cpp
@@ -0,0 +1,29 @@
+#include "camera.h"
+
+void camera::nossoLookat(
+	float eyeX, float eyeY, float eyeZ,
+	float centerX, float centerY, float centerZ,
+	float upX, float upY, float upZ)
+{
+
+}
+
+void camera::nossoRotate(float angle, float x, float y, float z)
+{
+
+}
+
+void camera::nossoTranslate(float x, float y, float z)
+{
+
+}
+
+void camera::produto_vetorial(float* prod, float* v1, float* v2)
+{
+
+}
+
+void camera::normalizar(float* v)
+{
+
+}
