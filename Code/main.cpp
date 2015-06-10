78372f469a22dd6c6d5aa43e899f4c7d0dd0da5a
Luís Delgado <lhds@cin.ufpe.br>
2015-06-10 19:25:00 -0300

    O tubarão tá brilhando *-*

--- Code/main.cpp
+++ Code/main.cpp
@@ -83,6 +83,22 @@ void Inicializa()
 
 	// Ativa o uso da luz ambiente 
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+
+	// Define os parâmetros da luz de número 0
+	glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular);
+	glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
+
+	// Habilita a definição da cor do material a partir da cor corrente
+	glEnable(GL_COLOR_MATERIAL);
+	//Habilita o uso de iluminação
+	glEnable(GL_LIGHTING);
+	// Habilita a luz de número 0
+	glEnable(GL_LIGHT0);
+	// Habilita o depth-buffering
+	glEnable(GL_DEPTH_TEST);
+	//-----fimIluminação
 }
 
 void TeclaPressionada(unsigned char tecla, int x, int y)
